70e5e1c9ba11fe98b63bd608baea3db3f437ee51
Selva <k.n.selvakumar@gmail.com>
2010-04-05 11:27:47 +0530

    I can 'choucheet' nowgit st

---
+++ javascripts/couchitter.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+  var loggedInUser = null;
+  
+  var refreshMyCoucheets = function(){
+    $.jcouch.getDocument({path:"_design/couchitter/_view/byUser?key=\"admin\""},
+    function(data){
+      var list_html = "";
+      sortedByDate = data.rows.sort(function(a,b){
+          return(new Date(b.value.createdAt) - new Date(a.value.createdAt));
+      });
+      
+      $(sortedByDate).each(function(index, coucheet){
+        date = new Date(coucheet.value.createdAt);
+        list_html += "<li>" + coucheet.value.value + " - " + date.toLocaleDateString() + " " + date.toLocaleTimeString()  + "</li>";
+      });
+      $("#coucheets").html(list_html);
+    })
+  }
+  
+  var afterLogin = function(){
+    $.jcouch.getLoggedInUser(function(name){
+      loggedInUser = name;
+      $("#login").fadeOut(function(){
+        $("#login").html("Welcome " + name + " !!");
+        $("#login").fadeIn();
+      });
+      
+      refreshMyCoucheets();
+    });
+  }
+
+  $("#login_form").submit(function(){
+    $.jcouch.login(
+      $("#username").attr("value"),
+      $("#password").attr("value"), 
+      afterLogin,ich
+      function(){
+        var error_message = $("#login_form #error_message")
+        error_message.text("Your are not authorized!!");
+        error_message.show();
+        setTimeout(function(){
+          error_message.fadeOut(function(){
+            $("#unauthorized_message").text("");
+          });
+          }, 3000);
+        }
+      );
+
+      return false;
+    });
+
+    var showPleaseLoginMessage = function(){
+      alert("Please login to do this.")
+    };
+
+    $("#coucheet").click(function(){
+      $.jcouch.getUUID(function(uuid){
+        $.jcouch.putDocument({
+          data:{
+            type : "Coucheet",
+            value : $("#happening_message").attr("value"),
+            user : loggedInUser,
+            createdAt : new Date()
+          },
+          path : uuid
+        },
+        refreshMyCoucheets,
+        showPleaseLoginMessage
+      );
+    });
+  });
+
+  $.jcouch.init({db : "couchitter"});
+
+  afterLogin();
+});
--- javascripts/jcouchquery.js
+++ javascripts/jcouchquery.js
@@ -9,14 +9,38 @@
   $.jcouch = {
     login : function(name, password, callback, failure_callback){
       jQuery.ajax({
-        url : this.couchDatabase.connectionString + "_session",
+        url : "/_session",
         data :{
           name : name,
           password : password
         },
         type : "POST",
         success : callback,
-        failure : failure_callback
+        error : failure_callback,
+      });
+    },
+    
+    getLoggedInUser : function(callback, failure_callback){
+      jQuery.ajax({
+        url : "/_session",
+        type : "GET",
+        dataType : "json",
+        success : function(data){
+          userName = data["userCtx"]["name"]
+          if(userName != null) { callback(userName) };
+        },
+        error : failure_callback
+      });
+    },
+    
+    getUUID : function(callback, failure_callback){
+      return jQuery.ajax({
+        url : "/_uuids",
+        dataType : "json",
+        success : function(data){
+          callback(data["uuids"][0]);
+        },
+        error : failure_callback
       });
     },
     
@@ -25,42 +49,44 @@
         url : _constructUrl(name),
         type : "POST",
         success : callback,
-        failure : failure_callback
+        error : failure_callback
       });
     },
 
     getDocument : function(doc_options, callback, failure_callback){
       jQuery.ajax({
-        url : couchDatabase.connectionString + _constructUrl(doc_options.name, doc_options.id),
+        url : this._constructUrl(doc_options.path),
         type : "GET",
+        dataType : "json",
         success : callback,
-        failure : failure_callback
+        error : failure_callback
       });
     },
 
-    // putDocument : function(doc_options, callback, failure_callback){},
-    // 
-    // deleteDocument : function(doc_options, callback, failure_callback){},
-    // 
-    // updateDocument : function(doc_options, callback, failure_callback){},
+    putDocument : function(doc_options, callback, failure_callback){
+      jQuery.ajax({
+        url : this._constructUrl(doc_options.path),
+        data : JSON.stringify(doc_options.data),
+        type : "PUT",
+        dataType : "json",
+        success : callback,
+        error : failure_callback
+      });
+    },
     
     _constructUrl : function(){
-      if (typeof(arguments) === "object") {
         var str = this.couchDatabase.connectionString();
         $(arguments).each(function(index, arg){
            str += "/" + arg;
         });
         return str;
-      } else {
-        return this.couchDatabase.connectionString() + "/" + arguments;
-      }
     },
     
     init: function(options){
       this.couchDatabase = new CouchDatabase(options.db);
       var username = options.username;
       var password = options.password;
-      if(username !== null && password !== null){ this.login(username, password); }
+      if(username != null && password != null){ this.login(username, password); }
     }
   }
 })(jQuery);
